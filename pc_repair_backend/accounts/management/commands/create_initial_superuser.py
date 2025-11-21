b2c44504c4ea39b8d704a0fcac07aee7a158afc8
iheblam <iheblamouchi.ik@gmail.com>
2025-11-21 01:51:31 +0100

    Add auto-create superuser command on startup

---
+++ pc_repair_backend/accounts/management/commands/create_initial_superuser.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from accounts.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create initial superuser if it does not exist'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@techcare.com',
+                password='Admin@123456',
+                first_name='Admin',
+                last_name='User'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser "admin" created successfully!'))
+            self.stdout.write(self.style.WARNING('Username: admin'))
+            self.stdout.write(self.style.WARNING('Password: Admin@123456'))
+            self.stdout.write(self.style.WARNING('CHANGE THIS PASSWORD IMMEDIATELY!'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Superuser already exists'))
